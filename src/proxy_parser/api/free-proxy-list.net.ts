26f9ee40c46aeeefe274b6ed7a6b45d263119f79
criempie <crimsnemptnss@yandex.ru>
2023-11-14 23:40:13 +0500

    created parser for free-proxy-list.net

---
+++ src/proxy_parser/api/free-proxy-list.net.ts
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { JSDOM } from 'jsdom';
+import { Logger } from '../../logger';
+import { Proxy } from '../../proxy_checker';
+import { common_headers } from '../common_headers';
+
+export class FreeProxyListNet {
+    private static _url = 'https://free-proxy-list.net/';
+    private static _logger = new Logger('parser free-proxy-list.net');
+
+    public static async load(): Promise<Proxy[] | false> {
+        try {
+            const page = await FreeProxyListNet._fetchPage();
+
+            return FreeProxyListNet._parsePage(page);
+        } catch (e) {
+            if (e instanceof Error) {
+                FreeProxyListNet._logger.error(e.message);
+
+                return false;
+            } else throw e;
+        }
+
+    }
+
+    private static async _parsePage(page_string: string): Promise<Proxy[] | false> {
+        try {
+            const page = new JSDOM(page_string);
+
+            const trs = page.window.document
+            .querySelector('table')!
+            .querySelector('tbody')!
+            .querySelectorAll('tr')!;
+
+            const proxies: Proxy[] = [];
+
+            for (const tr of trs) {
+                const tds = tr.querySelectorAll('td');
+
+                const ip = tds.item(0).innerHTML;
+                const port = +tds.item(1).innerHTML;
+                const isHttpsString = tds.item(6).innerHTML;
+
+                if (
+                    isHttpsString === 'no' || isHttpsString === 'yes'
+                    && !isNaN(port)
+                ) {
+                    proxies.push({
+                        protocol: isHttpsString === 'yes' ? 'https' : 'http',
+                        host: ip,
+                        port: port,
+                    });
+                }
+            }
+
+            return proxies;
+
+        } catch (e) {
+            if (e instanceof Error) {
+                FreeProxyListNet._logger.error(e.message);
+
+                return false;
+            } else throw e;
+        }
+    }
+
+    private static async _fetchPage(): Promise<string> {
+        return axios.get<string>(FreeProxyListNet._url, {
+            headers: common_headers,
+        }).then((r) => r.data);
+    }
+}
---
+++ src/proxy_parser/common_headers.ts
@@ -0,0 +1,11 @@
+export const common_headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Pragma': 'no-cache',
+    'Referer': 'https://yandex.ru/',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'cross-site',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0',
+};
