5a0bc4e2aacfd2ff88b3176540993089ff33cf8f
criempie <crimsnemptnss@yandex.ru>
2023-12-02 16:53:17 +0500

    add counter and children to logger

--- src/logger.ts
+++ src/logger.ts
@@ -2,31 +2,50 @@ import chalk, { ChalkFunction } from 'chalk';
 import { inspect } from 'util';
 
 export class Logger {
-    private static _chalk: chalk.Chalk = new chalk.Instance({ level: 1 });
+    protected static _chalk: chalk.Chalk = new chalk.Instance({ level: 1 });
 
-    private readonly _location: string;
+    private readonly _location_value: string;
 
-    constructor(location: string) {
-        this._location = location;
+    protected get _location(): string {
+        return this._location_value;
+    }
+
+    protected _previousLocations: string[];
+
+    protected get _fullLocation(): string[] {
+        return this._previousLocations.concat(this._location);
+    }
+
+    constructor(location: string, previousLocations: string[] = []) {
+        this._location_value = location;
+        this._previousLocations = previousLocations;
     }
 
     public static makeUnderline(message: string): string {
         return Logger._chalk.underline(message);
     }
 
+    public createChild(location: string): Logger {
+        return new Logger(location, this._fullLocation);
+    }
+
+    public createCounter(max: number): LoggerCounter {
+        return new LoggerCounter(this._fullLocation, max);
+    }
+
     public log(...messages: any): void {
-        Logger._log(this._location, messages);
+        Logger._log(this._fullLocation, messages);
     }
 
     public error(...messages: any): void {
-        Logger._log(this._location, messages, Logger._chalk.redBright);
+        Logger._log(this._fullLocation, messages, Logger._chalk.redBright);
     }
 
     public happy(...messages: any): void {
-        Logger._log(this._location, messages, Logger._chalk.greenBright);
+        Logger._log(this._fullLocation, messages, Logger._chalk.greenBright);
     }
 
-    private static _log(location: string, messages: any, colorFn?: ChalkFunction): void {
+    protected static _log(locations: string | string[], messages: any, colorFn?: ChalkFunction): void {
         const _messages = messages.map((m: any) => {
             let msg = m;
 
@@ -43,6 +62,31 @@ export class Logger {
             return msg;
         });
 
-        console.log(`[${ location }]: ${ _messages.join(' ') }`);
+        let _location: string;
+
+        if (typeof locations === 'string') _location = `[${ locations }]`;
+        else {
+            _location = locations.reduce((acc, item, i, arr) => {
+                return acc + `[${ item }]`;
+            }, '');
+        }
+
+        console.log(`${ _location }: ${ _messages.join(' ') }`);
+    }
+}
+
+class LoggerCounter extends Logger {
+    private _count: number;
+    private readonly _max: number;
+
+    constructor(previousLocations: string[], max: number) {
+        super('', previousLocations);
+
+        this._count = 0;
+        this._max = max;
+    }
+
+    protected override get _location() {
+        return `${ ++this._count }/${ this._max }`;
     }
 }
